a611f3aad7245bc7ae570e082e2a18a130b1f26e
Emil Einarsen <emil@hagatun.se>
2022-03-13 23:39:54 +0100

    fix(Menu.tsx): adjusted theme of heading for Merriweather font

--- layout/Nav/Menu.tsx
+++ layout/Nav/Menu.tsx
@@ -1,17 +1,15 @@
 import { Box, List, ListItem } from '@chakra-ui/layout'
-import { Heading, HeadingProps, IconButton } from '@chakra-ui/react'
+import { Text, TextProps, IconButton, Button } from '@chakra-ui/react'
+import { Heading } from 'components/core/Heading'
 
 import { Link } from 'components/core/Link'
 import { HamburgerIcon } from 'components/icons/HamburgerIcon'
 import { LogoIcon } from 'components/icons/LogoIcon'
 
-export const Item: React.FC<HeadingProps & { isDrawer?: boolean }> = ({ isDrawer, ...props }) => 
-	<ListItem
-		className="nav__list-item"
-	>
-		<Heading
+export const Item: React.FC<TextProps & { isDrawer?: boolean }> = ({ isDrawer, ...props }) => 
+	<ListItem>
+		<Text
 			{...props}
-			variant={isDrawer ? "h4" : "h6"}
 			{...isDrawer?{display:'flex', justifyContent: 'center'}:null}
 		/>
 	</ListItem>
@@ -24,13 +22,17 @@ export const Item: React.FC<HeadingProps & { isDrawer?: boolean }> = ({ isDrawer
  */
 export const Menu = () => 
 	<>
-		<Link href="/" aria-label="Go to homepage" exact><LogoIcon fontSize="7xl" color="#C3A94D" /></Link>
+		<Link href="/" aria-label="Go to homepage" exact display="flex" alignItems="center">
+			<LogoIcon fontSize="7xl" color="#C3A94D" />
+			<Heading.H6 marginTop={0} marginLeft={2}>HAGATUN REVISION</Heading.H6>
+		</Link>
 		<Box as="nav" className="nav" aria-label="Main">
 			<List className="nav__list" orientation="horizontal" alignItems="center">
 				<Item className="d-not-mobile" as={props => <Link {...props} href="/news" exact>NYHETER</Link>} />
 				<Item className="d-not-mobile" as={props => <Link {...props} href="/about" exact>OM OSS</Link>} />
 				<Item className="d-not-mobile" as={props => <Link {...props} href="/contact" exact>KONTAKTA</Link>} />
 				<Item className="d-not-mobile" as={props => <Link {...props} href="/services" exact>TJÄNSTER</Link>} />
+				<Item className="d-not-mobile" as={props => <Button {...props} variant="outline">SÖK</Button>} />
 				<IconButton
 					size="lg"
 					aria-label="Open the menu"
